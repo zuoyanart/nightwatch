08c45045f20a956f0557abda2b726fa0bd38cb53
左盐 <huabinglan@163.com>
2016-05-06 18:51:26 +0800

    修正部署脚本

--- depoly/depoly.sh
+++ depoly/depoly.sh
@@ -2,11 +2,11 @@
 # 检查nightwatch是否已经安装，没有安装则安装nightwatch
 # git clone项目所有源文件
 # 切换到项目目录，安装pkg
-# 启动Selenium 服务
+# 判断selenium是否启动，如果未启动，则启动Selenium 服务
 npm ls nightwatch || npm install nightwatch -g  && git clone https://github.com/zuoyanart/nightwatch.git newtest&& cd newtest && cnpm install&
-if  ps -ef | grep selenim
+if netstat -apn | grep 4444
 then
   echo "selenium is started"
 else
- && java -jar ./bin/selenium-server-standalone-2.53.0.jar
+  java -jar ./bin/selenium-server-standalone-2.53.0.jar
  fi
